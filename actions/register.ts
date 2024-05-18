4b5e363413e03015eb1ef7559ea2f1a0ef98baf1
Shunt <shunt_ps@hotmail.com>
2024-05-18 11:12:12 -0400

    Register user to database working - to continue

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
    const validatedFields = RegisterSchema.safeParse(values);
@@ -11,5 +14,18 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       return { error: "Champs invalides!" };
    }
 
-   return { success: "Votre compte à été créé" };
+   const { email, password, name } = validatedFields.data;
+   const hashedPassword = await bcrypt.hash(password, 10);
+
+   const existingUser = await getUserByEmail(email);
+
+   if (existingUser) {
+      return { error: "Cette adresse courriel est déjà utilisée" };
+   }
+
+   await db.user.create({ data: { name, email, password: hashedPassword } });
+
+   // TODO: Send verification token email
+
+   return { success: "Utilisateur créé avec succès" };
 };
