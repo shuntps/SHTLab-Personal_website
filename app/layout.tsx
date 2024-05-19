e636c8f035b7b17b933190bd1557eb99026efa6c
Shunt <shunt_ps@hotmail.com>
2024-05-19 07:32:06 -0400

    Add login/logout fonction

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="fr">
-         <head />
          <body className={inter.className}>{children}</body>
       </html>
    );
