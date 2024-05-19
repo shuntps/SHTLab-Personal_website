696b20b8c59e726df0cd36b628fa655bdde7b506
Shunt <shunt_ps@hotmail.com>
2024-05-19 06:40:43 -0400

    Add middleware for redirection

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="fr">
+         <head />
          <body className={inter.className}>{children}</body>
       </html>
    );
