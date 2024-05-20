cd1c7d09910473378fcc0ef5dc4820b8c66e9bd4
Shunt <shunt_ps@hotmail.com>
2024-05-20 08:27:18 -0400

    Fix error page when email already in use with other provider

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,19 @@
+// Not in use
+
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export const ErrorCard = () => {
+   return (
+      <CardWrapper
+         headerLabel="Oops! quelque chose s'est mal passé"
+         backButtonHref="/auth/login"
+         backButtonLabel="Retour à la connexion"
+      >
+         <div className="mt-3 w-full flex justify-center">
+            <ExclamationTriangleIcon className="text-destructive w-6 h-6" />
+         </div>
+      </CardWrapper>
+   );
+};
